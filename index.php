11d45df7e88bc60ef5acdcdc733b2cc279021466
vilashProgrammr <vilash@programmr.com>
2018-04-08 19:03:22 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if(isset($_GET['delete_id']))
 <script type="text/javascript">
 function edt_id(id)
 {
-	if(confirm('Sure to edit ?'))
+	if(confirm('Vilash take 4: Sure to edit ?'))
 	{
 		window.location.href='edit_data.php?edit_id='+id;
 	}
